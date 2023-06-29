2e72ffa4a43b1c12fd10dcd5133c5294becb70d5
Osvaldo Mesa <d3v.ozzy@gmail.com>
2023-06-29 12:54:01 -0700

    create required summary section elements in DOM

--- public/app.js
+++ public/app.js
@@ -1,8 +1,18 @@
-const summary_item = document.querySelector("#summary_item")
+const summary_items = document.querySelector("#summary_items")
 
 fetch("data.json")
   .then((response) => response.json())
   .then((jsonResponse) => {
     console.log(jsonResponse)
-    //jsonResponse is an array with objects within it
+    jsonResponse.forEach((object) => {
+      const summarySection = document.createElement("div")
+      const sectionImg = document.createElement("img")
+      const sectionTitle = document.createElement("div")
+      const sectionScore = document.createElement("div")
+      const scoreTotal = document.createElement("span")
+
+      console.log(object.category)
+      console.log(object.score)
+      console.log(object.icon)
+    })
   })
